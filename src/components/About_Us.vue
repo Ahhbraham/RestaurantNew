64992abf6c989419560774252095176cdeba488f
Abraham Muthiani <muthianiabraham254@gmail.com>
2025-02-21 12:12:04 +0300

    working on menu

--- src/components/About_Us.vue
+++ src/components/About_Us.vue
@@ -1,6 +1,97 @@
 <template>
-    <h1>About</h1>
+<! - these are the photos on our homepage - !>
+<v-card>
+
+
+
+  <v-carousel>
+  <v-carousel-item
+    src="../public/Re.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="../public/Rest.jpg"
+    cover
+  ></v-carousel-item>
+
+
+  <v-carousel-item
+    src="../public/Restaura.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="../public/a.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="../public/ab.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="../public/abc.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="../public/abcs.jpg"
+    cover
+  ></v-carousel-item>
+</v-carousel>
+</v-card>
+
+
+
+    <v-container>
+        <v-row>
+            
+            <v-typography class="text-left text-h2 text-high-emphasis" >
+                
+     About Us
+                
+            </v-typography>
+        
+            
+                <v-typography class="text-left text-h5 font-italic text-medium-emphasis">
+                    At The Velvet Fork, we believe dining is an experience meant to be savored, not just a meal to be consumed. Nestled in the heart of the city, our restaurant is a celebration of culinary artistry and refined elegance. From the moment you step inside, you’ll be enveloped by a warm, inviting atmosphere—an ideal blend of modern sophistication and timeless charm. Every corner of The Velvet Fork tells a story of passion, dedication, and a commitment to perfection.
+
+Our menu is a carefully curated selection of the finest ingredients, showcasing the artistry of our renowned chefs. We source locally when possible, ensuring every dish is not only fresh and vibrant but also supports our community’s growers and artisans. Each plate is a masterpiece, thoughtfully crafted to provide an unforgettable dining experience that engages all the senses.
+
+Our signature cocktails are a perfect complement to your meal, designed to elevate the flavors of your dish while providing an indulgent escape from the ordinary. Our wine list, an impressive collection of handpicked selections, promises to pair beautifully with any choice from our menu, creating a harmonious balance between food and drink.
+
+At The Velvet Fork, we understand that dining is more than just food—it’s about creating memories. Whether it’s an intimate dinner, a celebration, or a special occasion, we promise to provide an unforgettable experience that will leave you eagerly anticipating your next visit. Welcome to a world of luxury, flavor, and exceptional service. Welcome to The Velvet Fork.
+                </v-typography
+            </v-col>
+        </v-row>
+    </v-container>
+            
+            
+            
+             
+            
+            
+   
+
     </template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     <script setup>
 
--- src/components/Menu_Menu.vue
+++ src/components/Menu_Menu.vue
@@ -26,11 +26,16 @@
 
                                 <v-card-subtitle>{{ item.description }}</v-card-subtitle>
                             </v-card-item>
-                            <v-img height="400px" width="700px" src="item" cover ></v-img>
+                            <v-img height="400px" width="700px" :src="item.image" cover ></v-img>
                     
 
                             <v-card-text>
                             {{ item.price }}
+                           
+                            <v-btn variant="elevated" @click="addToOrder(item)">
+                            Order 
+                            </v-btn>
+
                             </v-card-text>
                         </v-card>
                         </v-col>
@@ -39,7 +44,33 @@
                         </v-card>
                         </v-container>
 
+<v-container>
+        <v-card color="#CEB1BE" >
+            <v-card-title>
+                <div class="text-h6">Current order: {{totalOrderAmount}}</div>
+            </v-card-title>
+        </v-card>
+    </v-container>
+    <v-container>
+        <v-card class="mx-auto" elevation="24">
+            <v-container>
+                <v-row>
+                    <v-col cols="12" md="4" v-for="item in orderItems" :key=item>
+                        <v-card color="#CEB1BE">
+                            <v-card-item>
+                            <v-card-title>{{ item.name }}</v-card-title>
 
+                            <v-card-subtitle>{{ item.description }}</v-card-subtitle>
+                            </v-card-item>
+                            <v-img height="200px" :src="item.image" cover ></v-img>
+
+                            <v-card-text>{{ item.price }}</v-card-text>
+                        </v-card>
+                        </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
+    </v-container>
     </template>
 
     <script setup>
@@ -49,10 +80,10 @@ const menuItems =
 {
 name: "Tomahawk Ribeye",
 price: 2500,
-description: "Medium Rare",
-size: "None",
+description: "Medium Rare",   
+size: "None",      
 offers: "None",
-image: "https://unsplash.com/photos/a-basket-of-french-fries-sitting-on-top-of-a-wooden-table-ChXHveqrb28?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
+image: "./public/toms.jpeg"
 },
 {
 name: "Grilled Chicken Breast",
@@ -60,7 +91,7 @@ price: 3000,
 description: "Juicy,Smoked",
 size: "None",
 offers: "None",
-image: "https://unsplash.com/photos/sausage-on-black-round-pan-cSxpCQrRlo8"
+image: "./public/chicken.jpg"
 },
 {
 name: "Seared Salmon",
@@ -68,7 +99,7 @@ price: 2700,
 description: "Fillet",
 size: "Large",
 offers: "None",
-image: "https://unsplash.com/photos/a-basket-of-french-fries-sitting-on-top-of-a-wooden-table-ChXHveqrb28?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
+image: "./public/salmon.jpg"
 },
 {
 name: "Goat Curry",
@@ -76,7 +107,7 @@ price: 2900,
 description: "Herb Infused",
 size: "Large",
 offers: "None",
-image: "https://unsplash.com/photos/a-basket-of-french-fries-sitting-on-top-of-a-wooden-table-ChXHveqrb28?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
+image: "./public/goatmatata.jpeg"
 },
 {
 name: "French Fries",
@@ -84,7 +115,7 @@ price: 1200,
 description: "Best fries in town",
 size: "Large",
 offers: "None",
-image: "https://unsplash.com/photos/a-basket-of-french-fries-sitting-on-top-of-a-wooden-table-ChXHveqrb28?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
+image: "./public/fries.jpeg"
 },
 {
 name: "Ugali",
@@ -92,9 +123,26 @@ price: 4200,
 description: "Pronounced 'uh-gh-a-li",
 size: "None",
 offers: "None",
-image: "https://unsplash.com/photos/a-basket-of-french-fries-sitting-on-top-of-a-wooden-table-ChXHveqrb28?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
+image: "./public/ugali.jpeg"
 },
-
 ]
+var orderItems = []
+var totaiOrderAmount=0
+
+function calculateTotalAmount(price){
+    totalOrderAmount += price
+}
+
+function addToOrder(item){
+    orderItems.push(item)
+    calculateTotalAmount(item.price)
+}
+
+
+
+
+
+
+
 
 </script>
